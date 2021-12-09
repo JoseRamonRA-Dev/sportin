ed416121de1b29acd2d8e4892a01509817ff9a69
NRekits <64814187+NRekits@users.noreply.github.com>
2021-12-09 01:12:26 -0600

    bug de registro arreglado

--- Front End/src/app/contenido/registro/registro.component.ts
+++ Front End/src/app/contenido/registro/registro.component.ts
@@ -1,64 +1,116 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import {  Router } from '@angular/router';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
+import { CruduserService } from '../servicios/cruduser.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
   forma: FormGroup;
   datos: any;
-  constructor(private formBuilder: FormBuilder,
-    private router: Router){
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    public servicio: CruduserService
+  ) {
     this.forma = new FormGroup({
-      'nombre': new FormControl('', [Validators.required, Validators.minLength(3)]),
-      'apep': new FormControl('',Validators.required),
-      'apem': new FormControl('',Validators.required),
-      'codigo': new FormControl('',Validators.required),
-      'numinterior': new FormControl('',Validators.required),
-      'numexterior': new FormControl('',Validators.required),
-      'calle': new FormControl('',Validators.required),
-      'celular': new FormControl('',Validators.required),
-      'contra': new FormControl('',Validators.required),
-      'contra2': new FormControl('',Validators.required),
-     });
-
+      Nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      ApePat: new FormControl(''),
+      ApeMat: new FormControl(''),
+      cod: new FormControl('', Validators.required),
+      int: new FormControl(''),
+      ext: new FormControl('', Validators.required),
+      calle: new FormControl('', Validators.required),
+      Telefono: new FormControl('', Validators.required),
+      Email: new FormControl('', Validators.required),
+      Contrasena: new FormControl('', Validators.required),
+      contra2: new FormControl('', Validators.required),
+    });
   }
+  ngOnInit(): void {}
 
-  guardarCambios():void{
-    this.datos = this.forma.value
-    if(this.datos.contra == this.datos.contra2){
-      if(this.forma.invalid){
+  guardarCambios(): void {
+    this.datos = this.forma.value;
+    if (this.datos.Contrasena == this.datos.contra2) {
+      if (this.forma.invalid) {
         Swal.fire({
           icon: 'error',
           title: 'ERROR',
           text: 'Tienes que añadir todos los campos requeridos',
-          footer: 'Intenta de nuevo'
-        })
-       }else{
-        Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Registro realizado con éxito',
-          showConfirmButton: false,
-          timer: 1500
+          footer: 'Intenta de nuevo',
         });
-        this.router.navigate(["/home"]);
-       }
-      
-      
-    }else{
+      } else {
+        this.servicio.buscarCodigo(this.datos.cod).subscribe((res) => {
+          if (res[0]) {
+            console.log(res[0]._id);
+            let body1 = {
+              Nombre: this.datos.Nombre,
+              ApePat: this.datos.ApePat,
+              ApeMat: this.datos.ApeMat,
+              Email: this.datos.Email,
+              Telefono: this.datos.Telefono,
+              Contrasena: this.datos.Contrasena,
+            };
+            this.servicio.registrarUsuario(body1).subscribe((resp) => {
+              console.log(resp);
+              console.log(resp._id);
+              if (resp == 'Email registrado') {
+                Swal.fire({
+                  icon: 'error',
+                  title: 'ERROR',
+                  text: 'El email ingresado ya esta registrado',
+                  footer: 'Intenta de nuevo',
+                });
+              } else {
+                let body = {
+                  id_us: resp._id,
+                  cod: res[0]._id,
+                  calle: this.datos.calle,
+                  int: this.datos.int,
+                  ext: this.datos.ext,
+                };
+                this.servicio.crearDireccion(body).subscribe((respu) => {
+                  console.log(respu);
+                  Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title:
+                      'Registro realizado con éxito, puedes iniciar sesión',
+                    showConfirmButton: false,
+                    timer: 1500,
+                  });
+                  this.router.navigate(['/home']);
+                });
+              }
+            });
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: 'ERROR',
+              text: 'No se encontro el código postal ingresado',
+              footer: 'Intenta de nuevo',
+            });
+          }
+        });
+      }
+    } else {
       Swal.fire({
         icon: 'error',
         title: 'Contraseñas',
-        text: 'Las contraseñas no coinciden'
-      })
+        text: 'Las contraseñas no coinciden',
+      });
     }
-   }
-  ngOnInit(): void {
   }
-
 }
