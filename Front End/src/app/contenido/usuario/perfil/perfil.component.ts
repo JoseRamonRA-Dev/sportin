d516a1591e8440e0b135f16a962ee72b3ed99bce
MarieTSD <marie.serna11@gmail.com>
2021-12-08 22:28:32 -0600

    perfil

--- Front End/src/app/contenido/usuario/perfil/perfil.component.ts
+++ Front End/src/app/contenido/usuario/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CruduserService } from '../servicios/cruduser.service';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
-
-  constructor() { }
+  
+  id="61b181cd14cfb09a9a53458a"
+  user:any
+  constructor(private crudUser:CruduserService) { }
 
   ngOnInit() {
+    this.crudUser.obtenerUsuarioActualizar(this.id).subscribe(
+      data => {
+        this.user = data
+        console.log("USER: ",this.user)
+      }
+    )
   }
 
 }
